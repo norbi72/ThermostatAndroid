db7a6443af6da5682ed7305f6e2ae811ddd471f0
Oszlányi Norbert <norbi72@gmail.com>
2020-05-12 06:30:28 +0200

    Mqtt connection v0.1

--- app/src/main/java/hu/norbi/thermostat/MainActivity.java
+++ app/src/main/java/hu/norbi/thermostat/MainActivity.java
@@ -3,12 +3,57 @@ package hu.norbi.thermostat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import hu.norbi.thermostat.helper.MqttHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    MqttHelper mqttHelper;
+    private TextView statusView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        statusView = findViewById(R.id.statusView);
+
+        startMqtt();
+    }
+
+    private void startMqtt(){
+        mqttHelper = new MqttHelper(getApplicationContext());
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+                Log.w("mqtt","Connected " + b + " - " + s);
+                mqttHelper.sendMessage("thermostat/state/cmd", "{\"get\":\"uptime\"}");
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                Log.w("Debug", "Topic: " + topic + " Msg: " + mqttMessage.toString());
+                //dataReceived.setText(mqttMessage.toString());
+                if (topic.endsWith("/response")) {
+                    statusView.setText(mqttMessage.toString());
+                }
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
     }
 }
